d6e8622a4a656ca43eb2fed5ac8587a02b672f40
Ta Minh Tien <taminhtien1993@gmail.com>
2015-12-04 22:50:00 +0700

    Add db seed

--- travel-api/db/seeds.rb
+++ travel-api/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do |n|
+  Traveler.create!(
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    birthday: FFaker::IdentificationESCO.expedition_date,
+    gender: FFaker::Gender.maybe )
+end
